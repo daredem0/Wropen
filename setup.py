0688c2e23ba62f1795dd8fab4599ee550aa422a0
Florian <37475414+daredem0@users.noreply.github.com>
2023-06-15 09:56:34 +0200

    Update setup.py
    Fix version string

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 import subprocess
-
+import re
 import setuptools
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = subprocess.run(["git", "describe"], capture_output=True, text=True)
+_version = subprocess.run(["git", "describe"], capture_output=True, text=True)
+version = _version.stdout
+minmajhash = re.compile(r"(\d+).(\d+).(\d+)")
+match = re.search(minmajhash, version)
+if match is not None:
+    major = match.group(1)
+    print(f"Found major: {major}.")
+    minor = match.group(2)
+    print(f"Found minor: {minor}.")
+    patch = match.group(3)
+    print(f"Found patch: {patch}.")
+parsed_version = f'{major}.{minor}.{patch}'
 
 setuptools.setup(
     name="wropen",
-    version=version.stdout,
+    version=parsed_version,
     author="Florian Leuze",
     author_email="f.leuze@outlook.de",
     description="Minimal package to intercept Popen.communicate.",
